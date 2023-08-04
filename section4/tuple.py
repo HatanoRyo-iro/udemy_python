a68b48ba4155e4a8b810b278e786c80820fd931f
RyoHatano <ryohatano0809@gmail.com>
2023-08-04 11:05:48 +0900

    tuple type

---
+++ section4/tuple.py
@@ -0,0 +1,26 @@
+t = (1, 2, 3, 4, 1, 2)
+print(t)
+
+print(type(t))
+
+# t[0] = 100   # エラーになる
+
+print(t.count(1))
+
+print(t.index(3))
+
+t = ([1, 2, 3], [4, 5, 6])
+
+print(t)
+print(t[0][2])
+
+t = 1, 2, 3, 4, 5
+print(type(t))
+
+# タプルは,をつける必要がある
+
+new_tuple = (1, 2, 3) + (4, 5, 6)
+print(new_tuple)
+# new_tuple = (1) + (4, 5, 6)   # エラー
+new_tuple = (1,) + (4, 5, 6)
+print(new_tuple)
