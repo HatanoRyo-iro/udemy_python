ef19b46f5fd50dcea3128e9adf5b8de14b903fd8
RyoHatano <ryohatano0809@gmail.com>
2023-08-04 11:31:10 +0900

    dictionary type

---
+++ section4/dictionary_type.py
@@ -0,0 +1,19 @@
+d = {'x': 10, 'y': 20}
+print(d)
+print(type(d))
+print(d['x'])
+d['x'] = 100
+print(d)
+d['x'] = 'XXXX'
+print(d)
+
+d['z'] = 200
+print(d)
+
+d[1] = 10000
+print(d)
+
+
+dict(a=10, b=20)   # これでも生成できる
+print(dict(a=10, b=20))
+print(dict([('a', 10), ('b', 20)]))
