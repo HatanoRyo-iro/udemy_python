6aa7f8b62749633b9cb13ecb20236289db922cb7
RyoHatano <ryohatano0809@gmail.com>
2023-08-06 16:48:09 +0900

    for文

---
+++ section5/for.py
@@ -0,0 +1,31 @@
+some_list = [1, 2, 3, 4, 5]
+
+# i = 0
+# while i < len(some_list):
+#     print(some_list[i])
+#     i += 1
+
+for i in some_list:
+    print(i)
+    
+for s in 'abcde':
+    print(s)
+    
+for word in ['My', 'name', 'is', 'Mike']:
+    if word == 'name':
+        # break
+        continue
+    print(word)
+    
+    
+print('####################')
+
+# for else文
+
+for fruit in ['apple', 'banana', 'orange']:
+    if fruit == 'banana':
+        print('stop eating')
+        break   # elseは実行されない
+    print(fruit)
+else:
+    print('I ate all!')
