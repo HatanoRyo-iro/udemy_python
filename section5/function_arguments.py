dff656f346d6f54798b1272b6161c57c1783f878
RyoHatano <ryohatano0809@gmail.com>
2023-08-10 01:27:56 +0900

    デフォルト引数で気をつけること

--- section5/function_arguments.py
+++ section5/function_arguments.py
@@ -15,3 +15,40 @@ def menu2(entree='beef', drink='wine', dessert='ice'):
     print('dessert = ', dessert)
     
 menu2('chicken', dessert='chocolate')   # デフォルト引数を上書きできる
+
+
+print('####################')
+
+# デフォルト引数で気をつけること
+def test_func(x, l=[]):   # 暗黙の了解としてデフォルト引数でリストを与えるべきではない
+    l.append(x)
+    return l
+
+# x = [1, 2, 3]
+# r = test_func(100, x)   
+# print(r)
+
+# x = [1, 2, 3]
+# r = test_func(200, x)
+# print(r)
+
+r = test_func(100)
+print(r)
+
+r = test_func(100)
+print(r)
+
+
+
+# じゃあどうするか
+def test_func2(x, l=None):
+    if l is None:
+        l = []
+    l.append(x)
+    return l
+
+r = test_func2(100)
+print(r)
+
+r = test_func2(100)
+print(r)
