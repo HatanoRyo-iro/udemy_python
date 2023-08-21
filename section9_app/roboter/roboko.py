c7d1f2de39bec73feb9c1e1f31f272042ab8fd91
RyoHatano <ryohatano0809@gmail.com>
2023-08-21 22:05:34 +0900

    csvファイルの有無による分岐

--- section9_app/roboter/roboko.py
+++ section9_app/roboter/roboko.py
@@ -1,3 +1,5 @@
+import os
+import csv
 from termcolor import cprint
 
 def start():
@@ -11,10 +13,37 @@ def start():
         if name != '':
             break
         
-    cprint("==============================================", 'green')
-    cprint(f"{name}さん。どこのレストランが好きですか？", 'green')
-    cprint(f"{name}, which restaurant do you like?", 'green')
-    cprint("==============================================", 'green')
+    csv_path = 'roboter/ranking.csv'
+    is_file = os.path.isfile(csv_path)
+    
+    if is_file:
+        # print('ファイルあるよ')
+        with open(csv_path, 'r+') as rank_csv:
+            pass
+            
+    
+    else:
+        # print('ファイルなし＝だから作る処理を書いて')
+        with open(csv_path, 'w') as rank_csv:
+            fieldnames = ['NAME', 'COUNT']
+            writer = csv.DictWriter(rank_csv, fieldnames=fieldnames)
+            writer.writeheader()
+            
+            cprint("==============================================", 'green')
+            cprint(f"{name}さん。どこのレストランが好きですか？", 'green')
+            cprint(f"{name}, which restaurant do you like?", 'green')
+            cprint("==============================================", 'green')
+            
+            while(1):
+                like_restaurant = input()
+                Like_Restaurant = like_restaurant.capitalize()
+                if like_restaurant != '':
+                    break
+                
+            print(Like_Restaurant)
+            
+                
+                
     
         
     
