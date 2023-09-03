76c2787f5ee5c44fee051cbf8abf528617293176
RyoHatano <ryohatano0809@gmail.com>
2023-09-03 22:11:37 +0900

    Unittestのスキップ

--- section14_test/Unittest.py
+++ section14_test/Unittest.py
@@ -3,6 +3,8 @@ import unittest
 import calculation
 
 
+release_name = 'lesson'
+
 class CalTest(unittest.TestCase):
     def setUp(self):
         print('setup')
@@ -12,6 +14,8 @@ class CalTest(unittest.TestCase):
         print('clean up')
         del self.cal
     
+    #@unittest.skip('skip!')
+    @unittest.skipIf(release_name=='lesson', 'skip!!')
     def test_add_num_and_double(self):
         self.assertEqual(self.cal.add_num_and_double(1, 1), 4)
    
