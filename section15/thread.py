f15f26c234dadb020966fe237483012fb21fc030
RyoHatano <ryohatano0809@gmail.com>
2023-09-07 01:51:33 +0900

    生存中のThreadオブジェクト全てのリスト

--- section15/thread.py
+++ section15/thread.py
@@ -15,11 +15,21 @@ def worker2():
     logging.debug('end')
     
 if __name__ == '__main__':
-    t1 = threading.Thread(target=worker1)
-    t1.daemon = True
-    t2 = threading.Thread(target=worker2)
-    t1.start()
-    t2.start()
-    print('started')
-    t1.join()
-    t2.join()   # これは書いても書かなくてもいい（明示的に書いてるだけ）
+    
+    threads = []
+    for _ in range(5):
+        t = threading.Thread(target=worker1)
+        t.daemon = True
+        t.start()
+        threads.append(t)
+    for thread in threading.enumerate():
+        if thread is threading.current_thread():
+            print(thread)
+            continue
+        thread.join()
+    # t2 = threading.Thread(target=worker2)
+    # t1.start()
+    # t2.start()
+    # print('started')
+    # t1.join()
+    # t2.join()   # これは書いても書かなくてもいい（明示的に書いてるだけ）
